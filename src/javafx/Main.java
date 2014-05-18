f49d27b7d2327dfc3829c572c372fb706c78b1b1
Loïs Postula <lois.postula@ulb.ac.be>
2014-05-18 20:10:37 +0200

    Adding the generated jar in a tar.gz

--- src/javafx/Main.java
+++ src/javafx/Main.java
@@ -1,10 +1,15 @@
 package javafx;
 
+import org.controlsfx.dialog.Dialogs;
 import util.Database;
 import util.GenerateXML;
 import util.XMLParser;
 import javafx.application.Application;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Main extends Application {
 
@@ -17,21 +22,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+
         launch(args);
-        /*Database db = new Database();
-    	db.update("TRUNCATE TABLE `user`");
-    	db.update("TRUNCATE TABLE `comment`");
-    	db.update("TRUNCATE TABLE `feed`");
-    	db.update("TRUNCATE TABLE `contain`");
-    	db.update("TRUNCATE TABLE `feedsubscription`");
-    	db.update("TRUNCATE TABLE `friendship`");
-    	db.update("TRUNCATE TABLE `publication`");
-    	db.update("TRUNCATE TABLE `readstatus`");
-    	db.update("TRUNCATE TABLE `sharedpublication`");
-    	db.close();
-    	System.out.println("Done clearing bdd");
-    	XMLParser a = new XMLParser("xmltest.xml");*/
-    	//GenerateXML.run();
-    	//System.out.println("done");
     }
 }
